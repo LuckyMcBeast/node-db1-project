3b34049898a2b3edc8197ffb0e8962a973e18c4d
mCullen-mcclellan <>
2021-03-30 00:58:22 -0400

    accounts API actions created. MVP Met. Provided test have critical issues. Should be changed.

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,50 @@
+const Accounts = require('./accounts-model.js');
+const ExpressError = require('./expressError.js');
+const db = require('../../data/db-config.js')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const newAccount = req.body;
+  if (!newAccount.name && !newAccount.budget) {
+    next(new ExpressError('Must include both name and budget', 400));
+  }
+  else {
+    next();
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.body.name) {
+    next(new ExpressError('No Name Provided', 400));
+  }
+  try {
+    const doesNameExist = await Accounts.checkName(req.body.name);
+    console.log(doesNameExist[0].id);
+    console.log(req.params);
+    console.log(req.body);
+    console.log(req.params);
+    console.log(doesNameExist[0].id != req.params.id);
+    if (doesNameExist[0].id !== req.params.id) {
+      next(new ExpressError('Account name already in use.', 406));
+    }
+    next()
+  } catch (err) {
+    next()
+  }
 }
-
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Accounts.getById(req.params.id);
+    if (account) {
+      req.account = account;
+      next();
+    } else {
+      next(new ExpressError('Account ID not Found', 404));
+    }
+  }
+  catch (err) {
+    next(new ExpressError(err, 500))
+  }
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,33 @@
-const getAll = () => {
+const db = require('../../data/db-config.js')
+
+const getAll = async () => {
   // DO YOUR MAGIC
+  return await db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where({id})
 }
 
 const create = async account => {
   // DO YOUR MAGIC
+  return await db('accounts').insert(account)
 }
 
 const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  return await db('accounts').update(account).where({id});
+
 }
 
 const deleteById = async id => {
   // DO YOUR MAGIC
+  return await db('accounts').del().where({ id })
+}
+
+const checkName = async name => {
+  return await db('accounts').where({name});
 }
 
 module.exports = {
@@ -24,4 +36,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
+  checkName
 }
